7c26e7efcbc50ec62724eaafab44efbfef837d73
Ivana Peneva <55322951+IvanaPeneva@users.noreply.github.com>
2022-06-15 17:19:29 +0200

    SlicerApiBetter
    Slicer Api DHR Better Version

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
+import math
+
 from fastapi import FastAPI
 from typing import Optional
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-#http://127.0.0.1:8000/api/calculator/?neededMaterial=24593&price=0.007
-#neededMaterial is from the Gcode smth like EXTRUDER_TRAIN.0.MATERIAL.VOLUME_USED:24593
+#i used this calculator as a guidance https://www.omnicalculator.com/other/3d-printing
+#the diameter of the fillament is the nozzle diameter
+#elngth of the filament used for printing,
+#time must be in minutes
+#http://127.0.0.1:8000/api/calculator/?density=1&diameter=1&length=1&price=1&time=5&hourly_rate=13
 
 
 @app.get("/api/calculator/")
-async def calculator(neededMaterial: float, price: float):
+async def calculator(density: float, diameter: float, length: float, price: float, time:int, hourly_rate:float):
 
-    sumPrice=neededMaterial*price
+    sum_price_printer= density * math.pi * pow((diameter / 2), 2) * length * price
+    labour = time * hourly_rate
+    endprice=sum_price_printer+labour #would be must better if rounded to the bigger with math.ceil
+
+    #i would also add utilities( used electricity)
     return JSONResponse(
             content={
-                "calculatedPrice": sumPrice,
+                'ENDPRICE':endprice,
+
+                'calculatedPricePrinter': sum_price_printer,
+                'density': density,
+                'diameter': diameter,
+                'length': length,
                 'price for unit of the material': price,
-                'needed material': neededMaterial,
+
+                'labourCosts':labour,
+                'time': time,
+                'hourly_rate': hourly_rate
 
             },
             status_code=200)
+
+'''
+filament type	full name	density (g/cm³)
+ABS	Acrylonitrile Butadiene Styrene	1.05
+PLA	Polylactic Acid	1.27
+PETG	Polyethylene Terephthalate	1.25
+PETT	T-Glase filament	1.45
+HIPS	High Impact Polystyrene	1.04
+TPU	Thermoplastic Polyurethane	1.30
+'''
